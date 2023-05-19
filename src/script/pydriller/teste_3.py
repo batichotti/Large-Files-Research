318f190ab698c4f8236932db657f9cfb8ef746f5
Defendi <72807509+tiagodefendi@users.noreply.github.com>
2023-05-19 15:35:33 -0300

    Update teste_3.py

--- src/script/pydriller/teste_3.py
+++ src/script/pydriller/teste_3.py
@@ -1,17 +1,19 @@
 import pydriller as drl
 
-repositories = ['https://github.com/Wolfterro/Projetos-em-C', 'https://github.com/django/django', 'https://github.com/nodejs/node']
+repositories = ['https://github.com/nodejs/node']
 
 for repository in repositories:
     print(repository)
-    for commit in drl.Repository(repository).traverse_commits():
-#    for commit in drl.Repository(repository, only_no_merge=True).traverse_commits():
+#    for commit in drl.Repository(repository).traverse_commits():
+    for commit in drl.Repository(repository, only_no_merge=True).traverse_commits():
         hash = commit.hash
         print(f'Hash: {hash}')
         print(f'{commit.merge}')
-        for file in commit.modified_files :
-            file_name = file.filename
-            lines_balance = file.added_lines - file.deleted_lines
-            print(file_name)
-            print(lines_balance)
-            
+        try:
+            for file in commit.modified_files :
+                file_name = file.filename
+                lines_balance = file.added_lines - file.deleted_lines
+                print(file_name)
+                print(lines_balance)
+        except Exception:
+            print('Error')
