51e4a42e6dc6efcf6bfc0c6cd39994ffebe81929
Mateus CohuzEr <52006006+MateusCohuzer@users.noreply.github.com>
2023-05-21 07:44:59 -0300

    fix all driller bugs

--- src/script/pydriller/driller.py
+++ src/script/pydriller/driller.py
@@ -3,91 +3,53 @@ import pandas as pd
 
 
 dataframe = []
-def dataFramer():
-    #global dataframe
+repositories = ['https://github.com/nodejs/node', 'https://github.com/matomo-org/matomo', 'https://github.com/gabrielfroes/webscraping_python_selenium']
+def dataFramer(commit, file):
     summary = pd.DataFrame({
-                'Hash': [hash],
-                'Project Name': [project_name],
-                'Local Commit PATH': [project_path],
-                'Message': [msg],
-                'Branch': [branch_name],
-                'Main Branch': [is_main_branch],
-                #'DMM size': [DMM_size],
-                #'DMM complexity': [DMM_complex],
-                #'DMM Interfacing': [DMM_interfacing],
-                'Number of Files': [num_files],
-                'Modified Lines': [mod_lines],
-                'File Name': [file_name],
-                'Change Type': [file_change_type],
-                'Local File PATH Old': [file_old_path],
-                'Local File PATH New': [file_new_path],
-                'Author Name': [author_name],
-                'Author Email': [author_email],
-                'Author Commit Date': [author_date],
-                'Author Commit Timezone' : [author_timezone],
-                'Committer Name': [committer_name],
-                'Committer Email': [committer_email],
-                'Committer Commit Date': [committer_date],
-                'Committer Timezone': [committer_timezone],
-                'Merge Commit': [is_merge_commit],
-                'Complexity': [file_complex],
-                'Methods': [file_methods],
-                #if len(file.methods) != 0:
-                #    print('Methods Before:')
-                #    for method in file.methods_before:
-                #        print(method)
-                #    print('Changed Method: ')
-                #    for method in file.changed_methods:
-                #        print(method)
-                'Tokens': [file_tokens],
-                'Lines Of Code(nloc)': [file_nloc],
-                'Lines Added': [file_lines_added],
-                'Lines Deleted': [file_lines_deleted],
-                'Lines Modified per File': [file_modified_lines],
-                'Lines Balance': [file_lines_balance]
-            })
+        'Hash': [commit.hash],
+        'Project Name': [commit.project_name],
+        'Local Commit PATH': [commit.project_path],
+        'Message': [commit.msg],
+        'Main Branch': [commit.in_main_branch],
+        'Number of Files': [commit.files],
+        'Modified Lines': [commit.lines],
+        'File Name': [file.filename],
+        'Change Type': [str(file.change_type).split('.')[-1]],
+        'Local File PATH Old': [file.old_path],
+        'Local File PATH New': [file.new_path],
+        'Author Name': [commit.author.name],
+        'Author Email': [commit.author.email],
+        'Author Commit Date': [commit.author_date],
+        'Author Commit Timezone': [commit.author_timezone],
+        'Committer Name': [commit.committer.name],
+        'Committer Email': [commit.committer.email],
+        'Committer Commit Date': [commit.committer_date],
+        'Committer Timezone': [commit.committer_timezone],
+        'Merge Commit': [commit.merge],
+        'Complexity': [file.complexity if file.complexity else 'null'],
+        'Methods': [len(file.methods)],
+        'Tokens': [file.token_count if file.token_count else 'null'],
+        'Lines Of Code(nloc)': [file.nloc if file.nloc else 'null'],
+        'Lines Added': [file.added_lines],
+        'Lines Deleted': [file.deleted_lines],
+        'Lines Modified per File': [file.added_lines + file.deleted_lines],
+        'Lines Balance': [file.added_lines - file.deleted_lines]
+    })
     dataframe.append(summary)
 
-
-for commit in Repository('https://github.com/matomo-org/matomo').traverse_commits():
-
-    for branch in commit.branches:
-        hash = commit.hash
-        project_name = commit.project_name
-        project_path = commit.project_path
-        msg = commit.msg
-        branch_name = branch
-        is_main_branch = commit.in_main_branch
-        #DMM_size = commit.dmm_unit_size
-        #DMM_complex = commit.dmm_unit_complexity
-        #DMM_interfacing = commit.dmm_unit_interfacing
-        num_files = commit.files
-        mod_lines = commit.lines
-        author_name = commit.author.name
-        author_email = commit.author.email
-        author_date = commit.author_date
-        author_timezone = commit.author_timezone
-        committer_name = commit.committer.name
-        committer_email = commit.committer.email
-        committer_date = commit.committer_date
-        committer_timezone = commit.committer_timezone
-        is_merge_commit = commit.merge
+repo = Repository(repositories[1])
+i = 0
+for commit in repo.traverse_commits():
+    if i == 0:
+        csv_name = commit.project_name
+    try:
         for file in commit.modified_files:
-            file_name = file.filename
-            file_change_type = str(file.change_type).split('.')[-1]
-            file_old_path = file.old_path
-            file_new_path = file.new_path
-            file_complex = (file.complexity if (str(type(file.complexity)).split("\'")[1] != "NoneType") else 'null')
-            file_methods = len(file.methods)
-            file_tokens = (file.token_count if (str(type(file.token_count)).split("\'")[1] != "NoneType") else 'null')
-            file_nloc = (file.nloc if (str(type(file.nloc)).split("\'")[1] != "NoneType") else 'null')
-            file_lines_added = file.added_lines
-            file_lines_deleted = file.deleted_lines
-            file_modified_lines = (file.added_lines + file.deleted_lines)
-            file_lines_balance = (file.added_lines - file.deleted_lines)
+            dataFramer(commit, file)
+            print(commit.committer_date)
+    except Exception:
+        i += 1
+        print("Error")
 
-        dataFramer()
-
+print(f"ERRORS {i}")
 summary_df = pd.concat(dataframe, ignore_index=True)
-
-summary_df.to_csv(f'src\\csvs\\Commits\\summary{commit.project_name}.csv', index=False)
+summary_df.to_csv(f'src\\csvs\\Commits\\summary_{csv_name}.csv', index=False)
