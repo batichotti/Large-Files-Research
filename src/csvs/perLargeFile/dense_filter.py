4dfc443ad619f43565206f538a4264a7eec481e0
Mateus CohuzEr <52006006+MateusCohuzer@users.noreply.github.com>
2023-10-02 14:55:59 -0300

    Update dense_filter.py

--- src/csvs/perLargeFile/dense_filter.py
+++ src/csvs/perLargeFile/dense_filter.py
@@ -18,42 +18,31 @@ def classify_file_purpose(commit_message):
         'documentation': 'Documentation',
         'test': 'Test',
         'configuration': 'Configuration',
-        # Adicione mais palavras-chave e classificações conforme necessário
     }
 
-    # Classificação padrão se nenhuma palavra-chave for encontrada
     default_classification = 'Other'
 
-    # Verifique as palavras-chave na mensagem de commit e retorne a classificação correspondente
     for keyword, classification in keywords.items():
         if keyword in commit_message:
             return classification
 
     return default_classification
 
-# Aplicar a classificação de propósito de arquivo ao DataFrame original
 original_df['File Purpose'] = original_df['Message'].apply(classify_file_purpose)
 
-# Ordenar o DataFrame pelo nome do arquivo, autor e a data do commit
 original_df.sort_values(by=['File Name', 'Author Email', 'Author Commit Date'], inplace=True)
 
-# Calcular o sinal de crescimento ou diminuição de linhas para cada autor
 original_df['Lines Sinal'] = original_df.apply(lambda row: 'Grew' if row['Lines Added'] > row['Lines Deleted'] else 'Decreased', axis=1)
 
-# Calcular a quantidade total de linhas alteradas no final para cada autor
 total_lines_changed = original_df.groupby(['File Name', 'Author Email'])['Lines Modified per File'].sum().reset_index()
 
-# Mesclar o total de linhas alteradas de volta ao DataFrame original
 original_df = pd.merge(original_df, total_lines_changed, on=['File Name', 'Author Email'], suffixes=('', '_total'))
 
-# Renomear a coluna para 'Lines Trend'
 original_df.rename(columns={'Lines Modified per File_total': 'Lines Trend'}, inplace=True)
 
-# Salvar apenas as colunas criadas neste arquivo em um novo CSV
 output_file_name = 'src\\csvs\\perLargeFile\\results.csv'
 columns_to_save = ['File Name', 'File Purpose', 'Author Email', 'Lines Sinal', 'Lines Trend']
 created_df = original_df[columns_to_save]
 created_df.to_csv(output_file_name, index=False)
 
-# Resultados
 print("O arquivo 'results.csv' foi criado com as colunas relevantes.")
